297eafded8f3df0a3567d5fd733765ce037a5a1c
Guido Tello <lpmaleinn@gmail.com>
2022-11-25 12:02:33 -0300

    Removed Deprecated Google play

--- Assets/Scripts/Auth.cs
+++ Assets/Scripts/Auth.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System;
-using GooglePlayGames;
-using GooglePlayGames.BasicApi;
+//using GooglePlayGames;
+//using GooglePlayGames.BasicApi;
 public class Auth : MonoBehaviour
 {
 
@@ -14,48 +14,48 @@ public class Auth : MonoBehaviour
     {
         Debug.LogError("\nIntento de log in\n");
         //Logger.SendLog("\nLogged in successfully\n");
-        PlayGamesPlatform.Instance.Authenticate(success =>
-        {
-            if (success)
-            {
-                Debug.LogError("\nLogged in successfully\n");
-                //Logger.SendLog("\nLogged in successfully\n");
-            }
-            else
-            {
-                Debug.LogError("\nLogin Failed\n");
-                //Logger.SendLog("\nLogin Failed\n");
-            }
-        });
+        //PlayGamesPlatform.Instance.Authenticate(success =>
+        //{
+        //    if (success)
+        //    {
+        //        Debug.LogError("\nLogged in successfully\n");
+        //        //Logger.SendLog("\nLogged in successfully\n");
+        //    }
+        //    else
+        //    {
+        //        Debug.LogError("\nLogin Failed\n");
+        //        //Logger.SendLog("\nLogin Failed\n");
+        //    }
+        //});
 
     }
 
     static public void ShowAchievements()
     {
 
-        if (Application.platform == RuntimePlatform.Android)
-        {
-            if (PlayGamesPlatform.Instance.IsAuthenticated())
-            {
-
-                PlayGamesPlatform.Instance.ShowAchievementsUI();
-                //Logger.SendLog("\nMostrando logros\n");
-            }
-        }
+        //if (Application.platform == RuntimePlatform.Android)
+        //{
+        //    if (PlayGamesPlatform.Instance.IsAuthenticated())
+        //    {
+        //
+        //        PlayGamesPlatform.Instance.ShowAchievementsUI();
+        //        //Logger.SendLog("\nMostrando logros\n");
+        //    }
+        //}
     }
 
 
     static public void UnlockAchievement(string a)
     {
 
-        if (Application.platform == RuntimePlatform.Android)
-        {
-            if (PlayGamesPlatform.Instance.IsAuthenticated())
-            {
-                PlayGamesPlatform.Instance.ReportProgress(a, 100f, success => { });
-                //Logger.SendLog("\nLogro desbloqueado!\n");
-            }
-        }
+        //if (Application.platform == RuntimePlatform.Android)
+        //{
+        //    if (PlayGamesPlatform.Instance.IsAuthenticated())
+        //    {
+        //        PlayGamesPlatform.Instance.ReportProgress(a, 100f, success => { });
+        //        //Logger.SendLog("\nLogro desbloqueado!\n");
+        //    }
+        //}
     }
 
 }
